80b07dd7b0a7f6efa1a605e97438db798e7e68e5
Michael <michaelzhaojq@hotmail.com>
2018-11-12 21:46:05 +1300

    add common files

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react'
 import { View, Text} from 'react-native'
 import { Provider } from 'react-redux'
 import { createStore} from 'redux'
-import firebase from 'firebase'
+import firebase from '@firebase/app' //eslint-disable-line
 import reducers from './reducers'
 
 class App extends Component {
-  
+
   componentWillMount() {
     const config = {
       apiKey: 'AIzaSyAXmdy5DuF1DmA8V_Z_LYuuqRXsn4681_w',
